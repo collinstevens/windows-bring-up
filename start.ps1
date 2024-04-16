c2ced59d0df7f08624882718fd6508844f122964
Collin Stevens <github@collinstevens.com>
2024-04-16 15:38:20 -0600

    pull git config into separate ps script

--- start.ps1
+++ start.ps1
@@ -185,16 +185,6 @@ winget uninstall --name "3D Viewer" --exact
 
 wsl --install --no-distribution --no-launch
 
-git config --global user.name "Collin Stevens"
-git config --global user.email "github@collinstevens.com"
-git config --global init.defaultBranch master
-git config --global push.autoSetupRemote true
-git config --global core.autoclrf false
-git config --global rerere.enabled true
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-git config --global alias.changelog "log --pretty=format:'%h: %s%n%n    %b'"
-git config --global alias.markdown "log --reverse --pretty=format:'- **%h**: %s%n  - %b'"
-
 # Enable Windows Sandbox
 Enable-WindowsOptionalFeature -FeatureName "Containers-DisposableClientVM" -All -Online -NoRestart
 
