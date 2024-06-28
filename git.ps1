8eab1b2e31a0a6eab980b6475256501728f2d39f
Collin Stevens <github@collinstevens.com>
2024-06-27 19:50:41 -0600

    use rebase.updateRefs for git

--- git.ps1
+++ git.ps1
@@ -4,6 +4,7 @@ git config --global init.defaultBranch master
 git config --global push.autoSetupRemote true
 git config --global core.autoclrf false
 git config --global rerere.enabled true
+git config --global rebase.updateRefs true
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.changelog "log --pretty=format:'%h: %s%n%n    %b'"
 git config --global alias.markdown "log --reverse --pretty=format:'- **%h**: %s%n  - %b'"
